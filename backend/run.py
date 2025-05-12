5037654cbcdb86dd316c27293fc2bd5b8c584fc3
dablu <dablu10jan@gmail.com>
2025-05-12 21:15:54 +0530

    code updated

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,16 @@
 from app import create_app
+from flask_cors import CORS
 
 #create flask app
 app = create_app()
+
+core = CORS(
+    app,
+    supports_credentials=True,
+    allow_headers="*",
+    expose_headers="*",
+    origins="*"
+)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
